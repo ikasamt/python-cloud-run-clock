e783583dfb9f730a927321528d5c586ffdd8e96c
ikasamt <ikasamt@gmail.com>
2019-07-18 18:05:22 +0900

    first commit

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 import os
 from datetime import datetime
+import uuid
+import json
+
+INSTANCE_ID = str(uuid.uuid4())[0:8]
+counter = 0
 
 def application(env, start_response):
     start_response('200 OK', [('Content-type', 'text/plain; charset=utf-8')])
-    now = datetime.now()
-    return [now.strftime("%Y-%m-%d-%H:%M:%S")]
+    if env['PATH_INFO'] == "/":
+        global counter
+        counter += 1
+    payload = {"INSTANCE_ID": INSTANCE_ID, "pid": os.getpid(), "counter": counter,}
+    return json.dumps(payload)
